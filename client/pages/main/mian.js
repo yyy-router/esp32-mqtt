937fd2a91914870228c5162ccf25f8926d00fe50
hqy <1804384725@qq.com>
2023-08-04 16:00:51 +0800

    remove redundancy

--- client/pages/main/mian.js
+++ client/pages/main/mian.js
@@ -62,52 +62,9 @@ Page({
   closeDevice: function () {
     this.pubMessage(globalData.client, `{"msg":"设备关闭","value":"0"}`)
   },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload() {
     console.log("页面卸载，连接关闭！")
     globalData.client.end()
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
 })
